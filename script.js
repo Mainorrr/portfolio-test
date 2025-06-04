35bbdcafa590acfffd1252d8f492c1360149635f
Mainorrr <mainor.cas.v@gmail.com>
2025-06-04 07:42:18 -0600

    Adding tamalito slideshow

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(res => res.text())
         .then(html => document.getElementById('navbar-placeholder').innerHTML = html);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Fetch existing navbar
+    fetch('navbar.html')
+        .then(res => res.text())
+        .then(html => document.getElementById('navbar-placeholder').innerHTML = html);
+
+    const img = document.getElementById('tamalito-img');
+    if (img) {
+        const images = [
+            'resources/tamalito/1.jpg',
+            'resources/tamalito/2.jpg',
+            'resources/tamalito/3.jpg',
+            'resources/tamalito/4.jpg'
+        ];
+        let idx = 0;
+        let transitionDuration = 1000;
+        let changeInterval = 3000;
+        setInterval(() => {
+            img.style.opacity = 0;
+            setTimeout(() => {
+                idx = (idx + 1) % images.length;
+                img.src = images[idx];
+                img.style.opacity = 1;
+            }, transitionDuration); // match transition duration
+        }, changeInterval); // change image every 3 seconds
+    }
+});
